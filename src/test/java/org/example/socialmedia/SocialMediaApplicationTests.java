8d3e5150b331696d6172efd7fd85a49d95bbdc40
Harsh <harshvardhan.yadav@sarvatra.in>
2024-04-08 13:46:20 +0530

    init

---
+++ src/test/java/org/example/socialmedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
